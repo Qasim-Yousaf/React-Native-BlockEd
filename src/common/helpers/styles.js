7223b0e98be39a3ff77655cdcb810575139cd78a
usama62 <usamaebrahim95@gmail.com>
2020-08-15 23:15:49 +0500

    first commit

---
+++ src/common/helpers/styles.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+import Constants from './Constants.json';
+
+const Styles = StyleSheet.create({
+  
+});
+
+export default Styles;
---
+++ src/screens/splash_screen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
+import SplashScreen from 'react-native-splash-screen'
+
+const style = StyleSheet.create({
+  safeAreaView: {
+    backgroundColor: AppColors.LightMode.BackgroundColor,
+    flex: 1,
+  }
+});
+
+function splash_screen() {
+  return{
+
+  }
+}
+
+export default splash_screen;
